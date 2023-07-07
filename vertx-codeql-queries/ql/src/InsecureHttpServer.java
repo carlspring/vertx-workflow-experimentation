6a2f3eb76322bc780317c4d8c1015ec32c7a87d0
Martin Todorov <carlspring@gmail.com>
2023-07-07 18:25:19 +0300

    Initial commit.

---
+++ vertx-codeql-queries/ql/src/InsecureHttpServer.java
@@ -0,0 +1,28 @@
+package org.carlspring.security.vertx.http;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.http.HttpServer;
+
+/**
+ * @author carlspring
+ */
+public class InsecureHttpServer
+        extends AbstractVerticle
+{
+
+    @Override
+    public void start()
+    {
+        // Create an insecure HTTP server
+        HttpServer server = vertx.createHttpServer();
+
+        // Configure server settings
+        server.requestHandler(request -> {
+            request.response().end("Hello, World!");
+        });
+
+        // Start the server
+        server.listen(8080);
+    }
+
+}
